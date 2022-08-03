1f5aeb032a10023d572f2534fb1e014e1b9240e8
NerbSerg <nigelsahl3@gmail.com>
2022-08-03 16:35:12 +0700

    ubahLib

--- tubes-magang2/main.cpp
+++ tubes-magang2/main.cpp
@@ -9,7 +9,12 @@
 
 #define SERVO PB_3
 
+DigitalOut led(LED1);
 servoKRAI servo(SERVO, 0.0);
+JoystickPS3 StickPS(PC_12,PD_2);
+
+float B1, B2, B3;
+
 
 FileHandle *mbed::mbed_override_console(int fd)
 {
@@ -17,19 +22,36 @@ FileHandle *mbed::mbed_override_console(int fd)
     return &serial_port;
 }
 
+
 int main()
 {
+    led=0;
+    StickPS.setup();
+    StickPS.stickState(&B1, &B2, &B3);
     while(true){
-        servo.position(50.0);
+        StickPS.baca_data();
 
-        printf("Nigel Sahl\n");
+        if (StickPS.getButtonDown() && StickPS.getLingkaran()){
+            led=0;   
+        }
+
+        if (StickPS.getButtonUp())
+            led=1; 
+
+        // if (StickPS.getLingkaran())
+
+        // servo.position(50.0);
+
+        // printf("Nigel Sahl\n");
         
-        wait_us(1000000);
-        servo.position(0.0);
+        // wait_us(1000000);
+        // servo.position(0.0);
 
-        printf("Nigel\n");
+        // printf("Nigel\n");
 
-        wait_us(1000000);
+        // wait_us(1000000);
+
+        // printf("%d %d %d/n", B1, B2, B3);
 
     }
     // printf("Hello, Mbed!\n");
